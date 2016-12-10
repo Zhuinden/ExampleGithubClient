f26fcab41b6e946b6d0ad824f9287adb73b29ef5
zhuinden <gabor.varadi@misoftware.hu>
2016-12-10 18:04:59 +0100

    reduce duplication in annotation cache

--- app/src/main/java/com/zhuinden/examplegithubclient/util/AnnotationCache.java
+++ app/src/main/java/com/zhuinden/examplegithubclient/util/AnnotationCache.java
@@ -7,6 +7,7 @@ import com.zhuinden.examplegithubclient.application.injection.MainActivityCompon
 import com.zhuinden.examplegithubclient.presentation.activity.main.MainActivity;
 import com.zhuinden.examplegithubclient.presentation.activity.main.MainKey;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -33,39 +34,41 @@ public class AnnotationCache {
 
     // from flow-sample: https://github.com/Zhuinden/flow-sample/blob/master/src/main/java/com/example/flow/pathview/SimplePathContainer.java#L100-L114
     private final Map<Class, Integer> PATH_LAYOUT_CACHE = new LinkedHashMap<>();
+    private final Map<Class, Integer> PATH_TITLE_CACHE = new LinkedHashMap<>();
 
-    public int getLayout(Object path) {
+    private <T, A> T getValueFromAnnotation(Map<Class, T> cache, Object path, Class<A> annotationClass) {
         Class pathType = path.getClass();
-        Integer layoutResId = PATH_LAYOUT_CACHE.get(pathType);
-        if(layoutResId == null) {
-            Layout layout = (Layout) pathType.getAnnotation(Layout.class);
-            if(layout == null) {
+        T value = cache.get(pathType);
+        if(value == null) {
+            A annotation = (A) pathType.getAnnotation(annotationClass);
+            if(annotation == null) {
                 throw new IllegalArgumentException(String.format("@%s annotation not found on class %s",
-                        Layout.class.getSimpleName(),
+                        annotationClass.getSimpleName(),
                         pathType.getName()));
             }
-            layoutResId = layout.value();
-            PATH_LAYOUT_CACHE.put(pathType, layoutResId);
+            try {
+                value = (T) annotationClass.getMethod("value").invoke(annotation);
+            } catch(IllegalAccessException e) {
+                // shouldn't happen
+                e.printStackTrace();
+            } catch(InvocationTargetException e) {
+                // shouldn't happen
+                e.printStackTrace();
+            } catch(NoSuchMethodException e) {
+                // shouldn't happen
+                e.printStackTrace();
+            }
+            cache.put(pathType, value);
         }
-        return layoutResId;
+        return value;
     }
 
     // from flow-sample: https://github.com/Zhuinden/flow-sample/blob/master/src/main/java/com/example/flow/pathview/SimplePathContainer.java#L100-L114
-    private static final Map<Class, Integer> PATH_TITLE_CACHE = new LinkedHashMap<>();
+    public int getLayout(Object path) {
+        return getValueFromAnnotation(PATH_LAYOUT_CACHE, path, Layout.class);
+    }
 
     public int getTitle(Object path) {
-        Class pathType = path.getClass();
-        Integer titleResId = PATH_TITLE_CACHE.get(pathType);
-        if(titleResId == null) {
-            Title title = (Title) pathType.getAnnotation(Title.class);
-            if(title == null) {
-                throw new IllegalArgumentException(String.format("@%s annotation not found on class %s",
-                        Title.class.getSimpleName(),
-                        pathType.getName()));
-            }
-            titleResId = title.value();
-            PATH_TITLE_CACHE.put(pathType, titleResId);
-        }
-        return titleResId;
+        return getValueFromAnnotation(PATH_TITLE_CACHE, path, Title.class);
     }
 }
